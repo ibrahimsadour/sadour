4d3801b4e5a9f0da6990cf6d357d374b93a21dfc
ibo <ibo@example.com>
2020-10-16 12:17:08 +0100

    seeder lang

--- database/seeds/HobbysSeeder.php
+++ database/seeds/HobbysSeeder.php
@@ -16,41 +16,35 @@ class HobbysSeeder extends Seeder
         DB::table('hobbys')->insert([
             [
                 'translation_lang' => 'en',
-                'translation_of' => 'en',
                 'name' => 'Muziek',
                 'slug' => 'ltr',
                 'active' => '1',
             ],
             [
                 'translation_lang' => 'en',
-                'translation_of' => 'en',
                 'name' => 'Gaming',
                 'slug' => 'ltr',
                 'active' => '1',
             ],           
             [
                 'translation_lang' => 'en',
-                'translation_of' => 'en',
                 'name' => 'Photography',
                 'slug' => 'ltr',
                 'active' => '1',
             ],           
             [
                 'translation_lang' => 'en',
-                'translation_of' => 'en',
                 'name' => 'Lezen',
                 'slug' => 'ltr',
                 'active' => '1',
             ],           [
                 'translation_lang' => 'en',
-                'translation_of' => 'en',
                 'name' => 'Reizen',
                 'slug' => 'ltr',
                 'active' => '1',
             ],           
             [
                 'translation_lang' => 'en',
-                'translation_of' => 'en',
                 'name' => 'Sport',
                 'slug' => 'ltr',
                 'active' => '1',
