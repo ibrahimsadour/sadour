48f05b6a8d03eef63c43a606fda3e732cd6c2e9c
ibo <ibo@example.com>
2020-10-19 10:45:36 +0100

    service

--- resources/views/sections/Skills.blade.php
+++ resources/views/sections/Skills.blade.php
@@ -1,59 +1,64 @@
 <!--Skills Sec-->
+<style>
+.skils{
+    font-weight: 500;
+}
+</style>
 <section id="skills_sec" class="skills-sec sec-pad-top-sm">
     <h2 class="mb-30">@lang('site.skills')</h2>
     <div class="row">
         <div class="col-md-3 col-sm-4 col-xs-6 mb-30">
             <div class="mdl-card mdl-shadow--2dp text-center pa-20 pt-30 pb-30 border-top-blue">
-                <i class=""></i>
-                <span>PHP</span>
+                <img class="" src="img/my partners/php.png" alt="Girl in a jacket" width="200px" height="90px">
+                <span class="skils">PHP</span>
             </div>
         </div>
 
         <div class="col-md-3 col-sm-4 col-xs-6 mb-30">
             <div class="mdl-card mdl-shadow--2dp text-center pa-20 pt-30 pb-30 border-top-green">
-                <i class=""></i>
-                <span>HTML</span>
+            <img class="" src="img/my partners/html.png" alt="Girl in a jacket" width="200px" height="90px">
+                <span class="skils">HTML</span>
             </div>
         </div>
         <div class="col-md-3 col-sm-4 col-xs-6 mb-30">
             <div class="mdl-card mdl-shadow--2dp text-center pa-20 pt-30 pb-30 border-top-blue">
-                <i class=""></i>
-                <span>CSS</span>
+            <img class="" src="img/my partners/css.png" alt="Girl in a jacket" width="200px" height="90px">
+                <span class="skils">CSS</span>
             </div>
         </div>
 
         <div class="col-md-3 col-sm-4 col-xs-6 mb-30">
             <div class="mdl-card mdl-shadow--2dp text-center pa-20 pt-30 pb-30 border-top-green">
-                <i class=""></i>
-                <span>JAVA SCRIPT</span>
+                <img class="" src="img/my partners/javascript.png" alt="Girl in a jacket" width="200px" height="90px">
+                <span class="skils">JAVA SCRIPT</span>
             </div>
         </div>
 
         <div class="col-md-3 col-sm-4 col-xs-6 mb-30">
             <div class="mdl-card mdl-shadow--2dp text-center pa-20 pt-30 pb-30 border-top-blue">
-                <i class=""></i>
-                <span>Communicatie</span>
+                <img class="" src="img/my partners/github.png" alt="Girl in a jacket" width="200px" height="90px">
+                <span class="skils">Github</span>
             </div>
         </div>
 
         <div class="col-md-3 col-sm-4 col-xs-6 mb-30">
             <div class="mdl-card mdl-shadow--2dp text-center pa-20 pt-30 pb-30 border-top-green">
-                <i class=""></i>
-                <span>Wordpress</span>
+                <img class="" src="img/my partners/WordPress.svg_.png" alt="Girl in a jacket" width="200px" height="90px">
+                <span class="skils">Wordpress</span>
             </div>
         </div>
 
         <div class="col-md-3 col-sm-4 col-xs-6 mb-30">
             <div class="mdl-card mdl-shadow--2dp text-center pa-20 pt-30 pb-30 border-top-blue">
-                <i class=""></i>
-                <span>SEO</span>
+                <img class="" src="img/my partners/Photoshop.png" alt="Girl in a jacket" width="200px" height="90px">
+                <span class="skils">SEO</span>
             </div>
         </div>
         
         <div class="col-md-3 col-sm-4 col-xs-6 mb-30">
             <div class="mdl-card mdl-shadow--2dp text-center pa-20 pt-30 pb-30 border-top-green">
-                <i class=""></i>
-                <span>Photoshop</span>
+                <img class="" src="img/my partners/seo.png" alt="Girl in a jacket" width="200px" height="90px">
+                <span class="skils">Photoshop</span>
             </div>
         </div>
     </div>
