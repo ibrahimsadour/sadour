e576144d2a3c764ad357b2dca2552f2e4139604e
ibo <ibo@example.com>
2020-10-19 10:52:21 +0100

    serviceSeeder

---
+++ database/seeds/ServiceSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Seeder;
+Use App\Models\Services;
+use Illuminate\Support\Facades\DB;
+class ServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('wat_ik_doe')->insert([
+            [
+                'titel' => 'Gebruikersomgeving',
+                'description' => 'Een mobiele eerste responsieve sjabloon met materiaalontwerp.',
+                'translation_lang' => 'en',
+                'translation_of' => '0',
+                'slug' => 'Gebruikersomgeving',
+                'active' => '1'
+
+            ],
+            [
+                'titel' => 'Branding',
+                'description' => 'Wordt geleverd met lichte en donkere opties. Geschikt voor het universum.',
+                'translation_lang' => 'en',
+                'translation_of' => '0',
+                'slug' => 'Branding',
+                'active' => '1'
+            ],
+            [
+                'titel' => 'Communicatie',
+                'description' => 'Buitengewoon instellingspaneel met veel geweldige functies.',
+                'translation_lang' => 'en',
+                'translation_of' => '0',
+                'slug' => 'Branding',
+                'active' => '1'
+
+            ],
+            [
+                'titel' => 'Strategie',
+                'description' => 'Eenvoudig aanpasbaar framework met bootstrap sass.',
+                'translation_lang' => 'en',
+                'translation_of' => '0',
+                'slug' => 'Branding',
+                'active' => '1'
+
+            ],
+
+        ]);
+    }
+}
